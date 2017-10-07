bedea10137badd62ee7ab79ee8a6e7b2a7ccf176
Lucas Rizzotto <contact@lucasrizzotto.com>
2017-10-07 13:59:48 -0400

    Main scene

---
+++ Assets/GoalManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoalManager : MonoBehaviour {
+
+    public bool IsGoalActive;
+
+    [Header("What gets activated if the goal is active?")]
+    public List<Animator> ThingsToActivate;
+   
+	public void UnlockGoal()
+    {
+        foreach(Animator anim in ThingsToActivate)
+        {
+            anim.SetTrigger("Activate");
+        }
+    }
+}
